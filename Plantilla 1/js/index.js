b70930d8e0a5cdb48f7e9ea1a923845f1b241812
luisalbalat <114280480+luisalbalat@users.noreply.github.com>
2024-04-05 02:40:12 +0200

    Add files via upload

---
+++ Plantilla 1/js/index.js
@@ -0,0 +1,43 @@
+window.sr = ScrollReveal();
+
+    sr.reveal('#navbar', {
+        duration: 2000,
+        origin: 'bottom',
+        distance: '-100px',
+    });
+
+    sr.reveal('#topbar', {
+        duration: 2000,
+        origin: 'bottom',
+        distance: '-100px',
+    });
+
+    sr.reveal('#carousel-text', {
+        duration: 2000,
+        origin: 'left',
+        distance: '-500px',
+    });
+
+    sr.reveal('#carousel-img', {
+        duration: 3000,
+        origin: 'right',
+        distance: '-400px',
+    });
+    
+    sr.reveal('#historia', {
+        duration: 2000,
+        origin: 'right',
+        distance: '-400px',
+    });
+
+    sr.reveal('#mision-vision', {
+        duration: 2000,
+        origin: 'left',
+        distance: '-400px',
+    });
+
+    sr.reveal('#footer', {
+        duration: 3000,
+        origin: 'top',
+        distance: '-100px',
+    });
